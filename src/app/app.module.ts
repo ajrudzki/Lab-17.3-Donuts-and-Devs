d92382dba3a5e5a1caeab1e707318787902c3030
Aaron Rudzki <aaronrudzki@quickenloans.com>
2020-10-15 17:15:27 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DonutsComponent } from './donuts/donuts.component';
+import { DonutDetailsComponent } from './donut-details/donut-details.component';
+import { FamousPersonDetailsComponent } from './famous-person-details/famous-person-details.component';
+import { FamousPeopleComponent } from './famous-people/famous-people.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DonutsComponent,
+    DonutDetailsComponent,
+    FamousPersonDetailsComponent,
+    FamousPeopleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
